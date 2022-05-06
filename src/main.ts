d7a5786c4c452ada035bdf487fb934463ef61e10
Shamim Hossain <hossains159@gmail.com>
2022-05-06 08:30:03 +0600

    chore: update swagger url prefix

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1', app, document);
 
   await app.listen(configService.get('app.port'));
 }
