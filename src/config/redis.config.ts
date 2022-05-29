d4625a302e74060a594cc3f17c8481ffa55d01de
Shamim Hossain <hossains159@gmail.com>
2022-05-29 20:43:30 +0600

    chore: update redis config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -14,6 +14,6 @@ export const cacheModuleOptions: CacheModuleAsyncOptions = {
 };
 
 export default registerAs('redis', () => ({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
 }));
